aa033ac0c9093d1e661fcca2288a0222c63669a2
pial Hasan <pialhasansu@gmail.com>
2024-07-03 12:37:52 +0600

    Add files via upload

---
+++ store5.js
@@ -0,0 +1,13 @@
+function handleSerach() {
+
+    const SearchInputElement = document.getElementById("search-input-filend");
+    const SearchInputValue = searchInputValue.value;
+    console.log(searchInputValue);
+    loadPhone(searchInputValue);
+}
+
+const  loadPhone = (searchText) =>{
+    console.log("Inside the Loadphone fuction :",searchText,);
+
+
+}
